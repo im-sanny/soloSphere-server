fca3fc999a8cdaa644fd21769956c5c8c4f17da2
im-sanny <raselparvezsanny@gmail.com>
2024-05-05 16:34:53 +0600

    .env added succesfully

---
+++ index.js
@@ -0,0 +1,43 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 6000
+
+const app = express()
+const corsOptions ={
+    origin:['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors({corsOptions}))
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.96corz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+     
+      
+     
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      
+      
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello from SoloSphere server...')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
