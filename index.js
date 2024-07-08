79366c0287cc8fe61ff4f1c4bc90c07c9ead25f3
im-sanny <raselparvezsanny@gmail.com>
2024-07-09 00:27:33 +0600

    add: added some job data fetching apis

--- index.js
+++ index.js
@@ -1,59 +1,102 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 9000
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 9000;
 
-const app = express()
-const corsOptions ={
-    origin:['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true,
-    optionSuccessStatus: 200,
-}
-app.use(cors({corsOptions}))
-app.use(express.json())
+const app = express();
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors({ corsOptions }));
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.96corz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  async function run() {
-    try {
-     const jobsCollection = client.db('soloSphereDB').collection('jobs')
-     const bidsCollection = client.db('soloSphereDB').collection('bids')
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const jobsCollection = client.db("soloSphereDB").collection("jobs");
+    const bidsCollection = client.db("soloSphereDB").collection("bids");
 
-     
-      
     //  get all jobs data from db
-    app.get('/jobs', async(req, res) =>{
-      const result = await jobsCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
 
     //get a single job data from db using id
-    app.get('/job/:id', async(req, res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await jobsCollection.findOne(query)
-      res.send(result)
-    })
-     
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      
-      
-    }
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // save a bid data in db
+    app.post("/bid", async (req, res) => {
+      const bidData = req.body;
+      const result = await bidsCollection.insertOne(bidData);
+      res.send(result);
+    });
+
+    // save a job data in db
+    app.post("/job", async (req, res) => {
+      const jobData = req.body;
+      const result = await jobsCollection.insertOne(jobData);
+      res.send(result);
+    });
+
+    //get all jobs posted by a specific user
+    app.get("/jobs/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "buyer.email": email };
+      const result = await jobsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //delete a job data from db
+    app.delete("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // update a job in db
+    app.put("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const jobData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...jobData,
+        },
+      };
+      const result = await jobsCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Hello from SoloSphere server...')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from SoloSphere server...");
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
